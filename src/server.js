195472ac6e2baf9f67ee6b74d3b9349077dffa4b
Kevin Bravo <79945749+mustafinho@users.noreply.github.com>
2022-12-21 12:14:45 +0000

    add specific permisions

--- src/server.js
+++ src/server.js
@@ -6,9 +6,10 @@ const app = express();
 const router = express.Router()
 
 router.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header('Access-Control-Allow-Headers: POST, GET, PUT, DELETE, OPTIONS, HEAD, authorization');
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
 });
 
