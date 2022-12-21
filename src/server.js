6570d1bea5eef77640a0012a87f7e3069c3a772f
Kevin Bravo <79945749+mustafinho@users.noreply.github.com>
2022-12-21 12:29:39 +0000

    idk

--- src/server.js
+++ src/server.js
@@ -6,11 +6,13 @@ const app = express();
 const router = express.Router()
 
 router.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header('Access-Control-Allow-Headers: POST, GET, PUT, DELETE, OPTIONS, HEAD, authorization');
-    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-    next();
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin,X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,PUT,DELETE,OPTIONS");
+        next();
 });
 
 router.get('/list-api', (req, res) => {
